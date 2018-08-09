d058ed7b3dc183200b81071b5e10f031c3446496
choigyumin <choigyumin@gmail.com>
2018-08-09 15:57:10 +0900

    structure change according to AwayDay github

--- realApp/__init__.py
+++ realApp/__init__.py
@@ -92,7 +92,7 @@ def signUp():
         return json.dumps({'html':'<span>Exception Occurred.</span>'})
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0")
 
 
